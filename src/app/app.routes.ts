35b102f5c94536b026ef0224e5e6391d18a847c7
Denta Muhajir <dentamuhajir@gmail.com>
2024-07-06 00:39:56 +0700

    Add other component and linking to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { OtherComponent } from './other/other.component';
 
 export const routes: Routes = [
 
    // { path: "", redirectTo: "", pathMatch: "full" },
     { path: "home", component: HomeComponent },
     { path: "root", component: AppComponent },
+    { path: "other", component: OtherComponent}
 ];
