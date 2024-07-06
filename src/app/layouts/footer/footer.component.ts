16b72dcd1c9f4d66de5d058d04cf86c07a5a93cb
Denta Muhajir <dentamuhajir@gmail.com>
2024-07-07 00:04:13 +0700

    Add style css & layouting

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
-
+export class FooterComponent implements OnInit {
+  currentYear : number = 2002;
+  constructor() {}
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear(); 
+  }
 }
