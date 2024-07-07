bb00aa899efc05a662b42ef9d50166d3cc7f5485
Denta Muhajir <dentamuhajir@gmail.com>
2024-07-07 13:35:19 +0700

    Fetching data detail product with param id

--- src/app/store/detail-product/detail-product.component.ts
+++ src/app/store/detail-product/detail-product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { DetailProductService } from './detail-product.service';
 
 @Component({
   selector: 'app-detail-product',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './detail-product.component.html',
   styleUrl: './detail-product.component.css'
 })
-export class DetailProductComponent {
-
+export class DetailProductComponent implements OnInit {
+  getID: number = 0
+  detailProduct: any 
+  constructor(private route: ActivatedRoute,private detailProductService: DetailProductService) {
+    route.params.subscribe( (param: Params) => {
+      let id = param["id"]
+      this.getID = id
+    })
+  }
+  ngOnInit(): void {
+    this.detailProductService.getDetailProduct(this.getID).subscribe( response => {
+      this.detailProduct = response
+    })
+    console.log(this.detailProduct)
+  }
 }
--- src/app/store/detail-product/detail-product.service.ts
+++ src/app/store/detail-product/detail-product.service.ts
@@ -1,12 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailProductService implements OnInit{
-
-  constructor() { }
+  endpoint: string = "https://api.escuelajs.co/api/v1/products/"
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
-    
+  }
+
+  getDetailProduct(id: number): Observable<any>{
+    console.log(id)
+    return this.http.get<any>(`${this.endpoint}`+ id)
   }
 }
